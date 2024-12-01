9e9eb97d3306bfba1beec63e5afbd09e085fa057
Pedro Kogawa <pedrokogawa@gmail.com>
2024-12-01 23:55:46 +0900

    add load_user

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 import os
+from dotenv import load_dotenv
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+Bootstrap5(app)
+
+# Configure Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
