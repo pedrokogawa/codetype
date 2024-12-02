90f9c11393952260ff7d5c754cfc47521ee7978e
Pedro Kogawa <pedrokogawa@gmail.com>
2024-12-03 00:33:54 +0900

    add mkdir and initial files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 import os
 from dotenv import load_dotenv
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
@@ -16,5 +18,19 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return db.get_or_404(User, user_id)
+  return db.get_or_404(User, user_id)
 
+
+# Create Database
+class Base(DeclarativeBase):
+  pass
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+@app.route('/')
+def home():
+  return render_template()
+
+if __name__ == "__main__":
+  app.run(debug=True, port=5001)
